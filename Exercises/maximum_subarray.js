c2b72d30d0e2dc9e10a3a5435f3a142bb2554ed4
ShazadO <shazadoutar8@gmail.com>
2024-02-10 01:12:40 -0500

    Another Idea for the Max Subarray Problem

--- Exercises/maximum_subarray.js
+++ Exercises/maximum_subarray.js
@@ -45,7 +45,7 @@ function maxSubarray(input) {
 	    // Next check if the sum of the current index value + it's next is > 0
 
 	    //if (input[left_index] + input[left_index + 1] > 0){
-
+	    
 	    // Add the value into the max subarray 
 	    max_subarray.push(input[left_index]);
 	    console.log(`\nPushing ${input[left_index]}\n`);
@@ -68,8 +68,48 @@ function maxSubarray(input) {
     return max_subarray;
 }
 
+function maxSubarrayV2(input) {
+    console.log(input);
+    // Idea 2: Similar to idea 1 but focus more on the sums instead of filtering out negatives
+    let left_index = 0;
+    let right_index = input.length - 1;
+    const max_subarray = [];
+    // TODO: Remove this after, it's just for testing
+    const left_array = [];
+    const right_array = [];
+    let sum = 0;
+    // Keep looping until the left and right meet
+    while (left_index < right_index) {
+	console.log(`\nLeft Index:${left_index} [${input[left_index]}], Right Index:${right_index} [${input[right_index]}]\n`);
+	// Handle the left side first
+	// If the sum of the current index and it's next is greater than 0, include it in the max subarray
+	console.log(`Sum of current left and next is ${input[left_index] + input[left_index + 1]}`)
+	if (input[left_index] + input[left_index + 1] > 0) {
+	    console.log(`Pushing index ${left_index}: ${input[left_index]}`);
+	    left_array.push(input[left_index]);
+	} else {
+	    // if the sum is < 0
+	}
+	left_index++;
+	// Handle the right side
+	// Same check idea
+	console.log(`Sum of current right and next is ${input[right_index] + input[right_index - 1]}`)
+	if (input[right_index] + input[right_index - 1] > 0) {
+	    console.log(`Pushing index ${right_index}: ${input[right_index]}`);
+	    right_array.push(input[right_index]);
+	}
+	right_index--;
+    }
+
+    //TODO: Remove this later
+    max_subarray[0] = left_array;
+    max_subarray[1] = right_array;
+    return max_subarray;
+}
+
 const array1 = [-2,1,-3,4,-1,2,1,-5,4];
-const array2 = [-2, 1, -3, 5]
+const array2 = [-2, 1, -3, 5];
+const array3 = [-2, 1, -3, 4, -1];
 // console.log(array1[-1]);
 // console.log(isNegative(array1, 0));
-console.log(maxSubarray(array1));
+console.log(maxSubarrayV2(array3));
